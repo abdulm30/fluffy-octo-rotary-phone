9b05af7b9c14735146ee3a887de244a87999d4ff
Abdul <abdulbasitm929@gmail.com>
2016-04-25 22:52:58 +0000

    first

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+#The basic Quote data model should have the following fields:
+#
+#author:string
+#
+#quote:text
+#
+#background:text
+#
+Quote.create(author:'Arthur C. Clarke',quote:'Any sufficiently advanced technology is indistinguisable from magic.',background:'Arthur C. Clarke')
+Quote.create(author:'Elon Musk',quote:'With artificialintelligence, we are summoning the demon.',background:'Now he makes electric cars.')
+Quote.create(author:'Pablo Picasso',quote:'Computers are useless. They can only give you answers.',background:'Some remarkable cubist paintings he did.')
